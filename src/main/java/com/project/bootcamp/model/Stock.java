0da017d42781617b66995e5ef0b795257a2abb89
Mariotti07 <midiasmart99@gmail.com>
2021-05-30 18:23:35 -0300

    feat: Uploading backend script

---
+++ src/main/java/com/project/bootcamp/model/Stock.java
@@ -0,0 +1,100 @@
+package com.project.bootcamp.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="price")
+	private Double price;
+	
+	@Column(name="variation")
+	private Double variation;
+
+	@Column(name="date")
+	private LocalDate date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getVariation() {
+		return variation;
+	}
+
+	public void setVariation(Double variation) {
+		this.variation = variation;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stock other = (Stock) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Stock() {
+	}
+
+}
---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+	
+	public static final String STOCK_ALREADY_EXITS = "Stock already exits in the database";
+	
+	public static final String STOCK_NOT_FOUND = "Stock not found in the database";
+
+}
